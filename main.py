84bba042db49c2b864ac18d4dcc84a5583de767c
Hirokoma Shaw <hamkuu@gmail.com>
2025-06-21 23:31:56 +0900

    lint Python Code

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from fasthtml.common import *
+from fasthtml.common import Div, P, fast_app, serve
 
-app,rt = fast_app()
+app, rt = fast_app()
 
-@rt('/')
-def get(): return Div(P('Hello from Movie Organizer!'), hx_get="/change")
+
+@rt("/")
+def get():
+    return Div(P("Hello from Movie Organizer!"), hx_get="/change")
+
 
 serve()
