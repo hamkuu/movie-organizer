868036734d21a32554e60df30f096ccacc23451b
Hirokoma Shaw <hamkuu@gmail.com>
2025-06-25 21:25:37 +0900

    check folder_dir validity

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ def get():
     return Div(P("Hello from Movie Organizer!"), hx_get="/change")
 
 
-@rt("/folders/{folder_path:path}")
-def get_folders(folder_path: Path):
-    return Div(*[P(item) for item in folder_path.iterdir()])
+@rt("/folders/{folder_dir:path}")
+def get_folders(folder_dir: Path):
+    if not folder_dir.exists():
+        return Div(P(f"Invalid {folder_dir=}: folder does not exist"))
+    if not folder_dir.is_dir():
+        return Div(P(f"Invalid {folder_dir=}: folder is not a directory"))
+    return Div(*[P(item) for item in folder_dir.iterdir()])
 
 
 serve()
