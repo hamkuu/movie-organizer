c74f86664a3667ca76f7c6b029bd50f94ec0903a
Hirokoma Shaw <hamkuu@gmail.com>
2025-06-25 21:19:47 +0900

    list folders

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from fasthtml.common import Div, P, fast_app, serve
 
 app, rt = fast_app()
@@ -6,6 +8,11 @@ app, rt = fast_app()
 @rt("/")
 def get():
     return Div(P("Hello from Movie Organizer!"), hx_get="/change")
+
+
+@rt("/folders/{folder_path:path}")
+def get_folders(folder_path: Path):
+    return Div(*[P(item) for item in folder_path.iterdir()])
 
 
 serve()
